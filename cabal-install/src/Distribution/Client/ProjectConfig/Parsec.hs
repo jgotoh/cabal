ff1784986292bf71ebca095a42bc97e8613e51a8
jgotoh <18658140+jgotoh@users.noreply.github.com>
2023-04-23 09:57:34 +0200

    Add parseSections stub to ProjectConfig.Parsec

--- cabal-install/src/Distribution/Client/ProjectConfig/Parsec.hs
+++ cabal-install/src/Distribution/Client/ProjectConfig/Parsec.hs
@@ -66,42 +66,38 @@ parseProjectSkeleton' lexWarnings utf8WarnPos fs = do
     parseWarnings (toPWarnings lexWarnings)
     for_ utf8WarnPos $ \pos ->
         parseWarning zeroPos PWTUTF $ "UTF8 encoding problem at byte offset " ++ show pos
-
-    -- TODO I am not sure whether we need sectionizeFields for cabal.project parsing
-    -- it is declared in Distribution.PackageDescription.Parsec.sectionizeFields
-    -- and used to convert old-style Cabal files, does this also apply to cabal.project files?
-    -- let (syntax, fs') = sectionizeFields fs
-    -- maybe we can also just use partitionFields
-
-    -- let (fields, sectionFields) = takeFields fs
-    parseCondTree specVer hasElif projectConfigFieldGrammar fs
+    parseCondTree specVer fs
     where
       -- TODO where do we get specVer?
       specVer :: CabalSpecVersion
       specVer = CabalSpecV3_8
-      hasElif = specHasElif specVer
 
 -- migrated from Distribution.PackageDescription.Parsec
 parseCondTree
     :: CabalSpecVersion
-    -> HasElif                                    -- ^ accept @elif@
-    -> ParsecFieldGrammar' ProjectConfig          -- ^ grammar
     -> [Field Position]
     -> ParseResult ProjectConfigSkeleton
-parseCondTree v hasElif grammar = go
-  where
-    go fields0 = do
-        let (fs, ss) = partitionFields fields0
+parseCondTree v fields0 = do
+        let (fs, sections) = partitionFields fields0
+            msg = show sections
+        parseWarning (Position 1 1) PWTInvalidSubsection msg
         imports <- parseImports fs
-        x <- parseFieldGrammar v fs grammar
-        branches <- concat <$> traverse parseIfs ss
-        return $ CondNode x imports branches
+        config <- parseFieldGrammar v fs projectConfigFieldGrammar
+        (config', branches) <- parseSections v config sections
+        return $ CondNode config' imports branches
 
+parseSections
+    :: CabalSpecVersion
+    -> ProjectConfig
+    -> [[Section Position]]
+    -> ParseResult (a, [CondBranch v [ProjectConfigImport] a])
+parseSections v config sections = undefined
+  where
     parseIfs :: [Section Position] -> ParseResult [CondBranch ConfVar [ProjectConfigImport] ProjectConfig]
     parseIfs [] = return []
     parseIfs (MkSection (Name _ name) test fields : sections) | name == "if" = do
         test' <- parseConditionConfVar test
-        fields' <- go fields
+        fields' <- parseCondTree v fields
         (elseFields, sections') <- parseElseIfs sections
         return (CondBranch test' fields' elseFields : sections')
     parseIfs (MkSection (Name pos name) _ _ : sections) = do
@@ -115,16 +111,16 @@ parseCondTree v hasElif grammar = go
     parseElseIfs (MkSection (Name pos name) args fields : sections) | name == "else" = do
         unless (null args) $
             parseFailure pos $ "`else` section has section arguments " ++ show args
-        elseFields <- go fields
+        elseFields <- parseCondTree v fields
         sections' <- parseIfs sections
         return (Just elseFields, sections')
 
-    parseElseIfs (MkSection (Name _ name) test fields : sections) | hasElif == HasElif, name == "elif" = do
+    parseElseIfs (MkSection (Name _ name) test fields : sections) | name == "elif" = do
         test' <- parseConditionConfVar test
-        fields' <- go fields
+        fields' <- parseCondTree v fields
         (elseFields, sections') <- parseElseIfs sections
         -- we parse an empty 'Fields', to get empty value for a node
-        a <- parseFieldGrammar v mempty grammar
+        a <- parseFieldGrammar v mempty projectConfigFieldGrammar
         return (Just $ CondNode a mempty [CondBranch test' fields' elseFields], sections')
 
     parseElseIfs (MkSection (Name pos name) _ _ : sections) | name == "elif" = do
