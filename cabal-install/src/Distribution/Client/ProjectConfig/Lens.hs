2e2fcf94e54cd83380d2f92aa76e4c2e6e3544fc
jgotoh <18658140+jgotoh@users.noreply.github.com>
2023-05-31 19:35:22 +0200

    Add more ProjectConfig lenses

--- cabal-install/src/Distribution/Client/ProjectConfig/Lens.hs
+++ cabal-install/src/Distribution/Client/ProjectConfig/Lens.hs
@@ -1,51 +1,70 @@
 module Distribution.Client.ProjectConfig.Lens where
 
 import Distribution.Compat.Prelude
-import Distribution.Client.ProjectConfig.Types (ProjectConfig (..), ProjectConfigBuildOnly, ProjectConfigShared, ProjectConfigProvenance, PackageConfig, MapMappend)
+import Distribution.Client.ProjectConfig.Types (ProjectConfig (..), ProjectConfigBuildOnly (..), ProjectConfigShared, ProjectConfigProvenance, PackageConfig, MapMappend)
 import Distribution.Compat.Lens
 import Distribution.Client.Types.SourceRepo (SourceRepoList)
 import qualified Distribution.Client.ProjectConfig.Types as T
 import Distribution.Package
          ( PackageName, PackageId, UnitId )
+import Distribution.Simple.Setup
+         ( Flag, HaddockTarget(..), TestShowDetails(..), DumpBuildInfo (..) )
 import Distribution.Types.PackageVersionConstraint
          ( PackageVersionConstraint )
+import Distribution.Verbosity
+import Distribution.Simple.InstallDirs
+         ( PathTemplate, InstallDirs )
+import Distribution.Utils.NubList
+         ( NubList, toNubList, fromNubList )
 
 projectPackages :: Lens' ProjectConfig [String]
 projectPackages f s = fmap (\x -> s { T.projectPackages = x }) (f (T.projectPackages s))
-{-# INLINE projectPackages #-}
+{-# INLINABLE projectPackages #-}
 
 projectPackagesOptional :: Lens' ProjectConfig [String]
 projectPackagesOptional f s = fmap (\x -> s { T.projectPackagesOptional = x }) (f (T.projectPackagesOptional s))
-{-# INLINE projectPackagesOptional #-}
+{-# INLINABLE projectPackagesOptional #-}
 
 projectPackagesRepo :: Lens' ProjectConfig [SourceRepoList]
 projectPackagesRepo f s = fmap (\x -> s { T.projectPackagesRepo = x }) (f (T.projectPackagesRepo s))
-{-# INLINE projectPackagesRepo #-}
+{-# INLINABLE projectPackagesRepo #-}
 
 projectPackagesNamed :: Lens' ProjectConfig [PackageVersionConstraint]
 projectPackagesNamed f s = fmap (\x -> s { T.projectPackagesNamed = x }) (f (T.projectPackagesNamed s))
-{-# INLINE projectPackagesNamed #-}
+{-# INLINABLE projectPackagesNamed #-}
 
 projectConfigBuildOnly :: Lens' ProjectConfig ProjectConfigBuildOnly
 projectConfigBuildOnly f s = fmap (\x -> s { T.projectConfigBuildOnly = x }) (f (T.projectConfigBuildOnly s))
-{-# INLINE projectConfigBuildOnly #-}
+{-# INLINABLE projectConfigBuildOnly #-}
 
 projectConfigShared :: Lens' ProjectConfig ProjectConfigShared
 projectConfigShared f s = fmap (\x -> s { T.projectConfigShared = x }) (f (T.projectConfigShared s))
-{-# INLINE projectConfigShared #-}
+{-# INLINABLE projectConfigShared #-}
 
 projectConfigProvenance :: Lens' ProjectConfig (Set ProjectConfigProvenance)
 projectConfigProvenance f s = fmap (\x -> s { T.projectConfigProvenance = x }) (f (T.projectConfigProvenance s))
-{-# INLINE projectConfigProvenance #-}
+{-# INLINABLE projectConfigProvenance #-}
 
 projectConfigAllPackages :: Lens' ProjectConfig PackageConfig
 projectConfigAllPackages f s = fmap (\x -> s { T.projectConfigAllPackages = x }) (f (T.projectConfigAllPackages s))
-{-# INLINE projectConfigAllPackages #-}
+{-# INLINABLE projectConfigAllPackages #-}
 
 projectConfigLocalPackages :: Lens' ProjectConfig PackageConfig
 projectConfigLocalPackages f s = fmap (\x -> s { T.projectConfigLocalPackages = x }) (f (T.projectConfigLocalPackages s))
-{-# INLINE projectConfigLocalPackages #-}
+{-# INLINABLE projectConfigLocalPackages #-}
 
 projectConfigSpecificPackage :: Lens' ProjectConfig (MapMappend PackageName PackageConfig)
 projectConfigSpecificPackage f s = fmap (\x -> s { T.projectConfigSpecificPackage = x }) (f (T.projectConfigSpecificPackage s))
-{-# INLINE projectConfigSpecificPackage #-}
+{-# INLINABLE projectConfigSpecificPackage #-}
+
+projectConfigVerbosity :: Lens' ProjectConfigBuildOnly (Flag Verbosity)
+projectConfigVerbosity f s = fmap (\x -> s { T.projectConfigVerbosity = x }) (f (T.projectConfigVerbosity s))
+{-# INLINABLE projectConfigVerbosity #-}
+
+projectConfigSummaryFile :: Lens' ProjectConfigBuildOnly (NubList PathTemplate)
+projectConfigSummaryFile f s = fmap (\x -> s { T.projectConfigSummaryFile = x }) (f (T.projectConfigSummaryFile s))
+{-# INLINABLE projectConfigSummaryFile #-}
+
+projectConfigSummaryFile' :: Lens' ProjectConfigBuildOnly [PathTemplate]
+projectConfigSummaryFile' f s = fmap (\x -> s { T.projectConfigSummaryFile = toNubList x }) (f (fromNubList $ T.projectConfigSummaryFile s))
+{-# INLINABLE projectConfigSummaryFile' #-}
